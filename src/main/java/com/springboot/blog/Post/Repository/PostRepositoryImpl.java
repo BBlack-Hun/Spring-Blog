a4c3d8668ad14319bf46b27d6dd68a9b93372eff
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-03-16 22:41:56 +0900

    Create Custom Exception - ResourceNotFoundException

--- src/main/java/com/springboot/blog/Post/Repository/PostRepositoryImpl.java
+++ src/main/java/com/springboot/blog/Post/Repository/PostRepositoryImpl.java
@@ -3,5 +3,5 @@ package com.springboot.blog.Post.Repository;
 import com.springboot.blog.Post.VO.ParamVO;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface PostRepositoryImpl extends MongoRepository<ParamVO, Long> {
+public interface PostRepositoryImpl extends MongoRepository<ParamVO, String> {
 }
